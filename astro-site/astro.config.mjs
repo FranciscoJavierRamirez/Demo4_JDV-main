0463eaa321d3fc4a008281ee1a14f02a150c2234
Frank <89029955+FranciscoJavierRamirez@users.noreply.github.com>
2026-01-19 04:18:46 -0300

    listo para publicarse en Cloudflare

--- astro-site/astro.config.mjs
+++ astro-site/astro.config.mjs
@@ -6,9 +6,9 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jdvabogados.cl',
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [react()]
 });
